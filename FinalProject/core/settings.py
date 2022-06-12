d116edadc1ceed7fa166cf7ad66778a99ab3144b
YeabAM <yeabkalu@gmail.com>
2022-06-12 11:56:08 +0300

    added cors in core/settings.py

--- FinalProject/core/settings.py
+++ FinalProject/core/settings.py
@@ -71,6 +71,7 @@ ACCOUNT_EMAIL_REQUIRED = True
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     "whitenoise.middleware.WhiteNoiseMiddleware",
+     "corsheaders.middleware.CorsMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -78,6 +79,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'core.urls'
 
